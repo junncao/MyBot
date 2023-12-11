e599cdfd97c0ff3317c40cb2eb79fe79ab539bc0
caojun05 <caojun05@kuaishou.com>
2023-12-11 22:54:52 +0800

    local

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-API_KEY = ""
-webhook = ""
+API_KEY_SB = "sb-91dd7e906a7e675931eb29d3ad30d5d6dbcd828650b78203"
+API_KEY_OPENAI = "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"
+WEB_HOOK = "https://kim-robot.kwaitalk.com/api/robot/send?key=a1f19682-5857-4a01-b1eb-33409d327e62"
